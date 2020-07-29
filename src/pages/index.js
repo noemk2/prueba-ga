27b6d6b5ee80a59b09a23e301ffc375be84b1c87
Noe Huaccharaque <noemk2@gmail.com>
2020-07-28 20:26:20 -0500

    add analitics

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,8 @@ const IndexPage = () => (
     <h1>Hi my name is Noe</h1>
     <p>Iam from developer</p>
     <p>Now go build something great.</p>
-	<Link to="/page-2/">Go to page 2</Link> <br />
+    <a to="https://bit.ly/noemk2">Go to my github</a> <br />
+    <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
